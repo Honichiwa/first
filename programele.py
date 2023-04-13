779c7a7cc9f9356b9b7046cb782f2ced8c5575a5
Honichiwa <tadas.zupka@gmail.com>
2023-04-13 12:02:59 +0300

    Update

--- programele.py
+++ programele.py
@@ -1,26 +1,26 @@
 
-print("")
-print("DC Watts-Amps-Watts calculator")
-print("")
-electrical_m = input(
-    "Type in conversion type: \n \n A - Watts to Amps \n W - Amps to Watts \n \n A/W? : ")
 
-if electrical_m == "A" or electrical_m == "a" \
-        or electrical_m == "W" or electrical_m == "w":
-    voltage = input("\n Type in systems voltage: ")
-    if int(voltage) > 0:
-        if electrical_m == "A" or electrical_m == "a":
-            watt = input("\n Type in Watts: ")
-            amp = float(watt) / float(voltage)
-            print("")
-            print(f"{watt} W is equal to {amp:.02f} A.")
-        elif electrical_m == "W" or electrical_m == "w":
-            amp = input("\n Type in Amps: ")
-            watt = float(amp) * float(voltage)
-            print("")
-            print(f"{amp} A is equal to {watt:.02f} W.")
-    else:
-        print("Incorrect voltage: restart program")
+print("\nDC Watts-Amps-Watts conversion")
+
+electrical_m = input(
+    "\nType in conversion type: \n \nA - Watts to Amps \nW - Amps to Watts \n \nA/W? : ")
+
+if electrical_m.lower() == "a" or electrical_m == "w":
+    try:
+        voltage = float(input("\nType in systems voltage: "))
+
+    except ValueError:
+        print("\nIncorrect voltage: restart program")
+
+    if electrical_m.lower() == "a":
+        watt = float(input("\nType in Watts: "))
+        amp = float(watt) / float(voltage)
+        print(f"\n{watt} W is equal to {amp:.02f} A in {voltage} Voltage DC system.")
+
+    elif electrical_m.lower() == "w":
+        amp = float(input("\nType in Amps: "))
+        watt = float(amp) * float(voltage)
+        print(f"\n{amp} A is equal to {watt:.02f} W in {voltage} Voltage DC system.")
 
 else:
-    print("Incorrect selection: restart program")
+    print("\nIncorrect selection: restart program")
